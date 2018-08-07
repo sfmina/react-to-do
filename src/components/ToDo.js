3b17f8c9c21fd565c9340785b8bb18cb33db460b
sfmina <minafranke@gmail.com>
2018-08-07 08:44:02 -0700

    Add button

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,10 +3,14 @@ import React, { Component } from 'react';
 class ToDo extends Component {
 	render() {
 		return (
-			<li>
-			<input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-			<span>{ this.props.description }</span>
-			</li>
+			<div>
+				<li>
+				<input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+				<span>{ this.props.description }</span> <input type="button" value="Delete"/>
+				</li>
+
+			</div>
+
 			);
 	}
 }
